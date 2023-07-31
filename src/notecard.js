d9c4b743a545e5efaf82d79f5b2673acd00ef8b7
Nyleec <nyleecp@gmail.com>
2023-07-31 15:26:16 -0700

    carousel "works"

--- src/notecard.js
+++ src/notecard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import NoteCardGrid from './NoteCardGrid'; // Import the NoteCardGrid component
 
 const NoteCardCarousel = () => {
   const noteCardsData = [
@@ -9,6 +10,7 @@ const NoteCardCarousel = () => {
   ];
 
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [viewMode, setViewMode] = useState('carousel'); // Added viewMode state
 
   const nextCard = () => {
     setCurrentCardIndex((currentCardIndex + 1) % noteCardsData.length);
@@ -18,22 +20,36 @@ const NoteCardCarousel = () => {
     setCurrentCardIndex((currentCardIndex - 1 + noteCardsData.length) % noteCardsData.length);
   };
 
+  const toggleViewMode = () => {
+    setViewMode(prevMode => (prevMode === 'carousel' ? 'grid' : 'carousel'));
+  };
+
   return (
     <div className="carousel-container">
-      <div className="note-cards-container">
-        <div className="note-card prev">
-          {noteCardsData[(currentCardIndex - 1 + noteCardsData.length) % noteCardsData.length]}
-        </div>
-        <div className={`note-card ${currentCardIndex}`}>
-          {noteCardsData[currentCardIndex]}
-        </div>
-        <div className="note-card next">
-          {noteCardsData[(currentCardIndex + 1) % noteCardsData.length]}
-        </div>
-      </div>
-      <div className="carousel-arrows">
-        <span className="arrow" onClick={prevCard}>&#10094;</span>
-        <span className="arrow" onClick={nextCard}>&#10095;</span>
+      {viewMode === 'carousel' ? (
+        <>
+          <div className="note-cards-container">
+            <div className="note-card prev">
+              {noteCardsData[(currentCardIndex - 1 + noteCardsData.length) % noteCardsData.length]}
+            </div>
+            <div className={`note-card ${currentCardIndex === 0 ? 'active' : ''}`}>
+              {noteCardsData[currentCardIndex]}
+            </div>
+            <div className="note-card next">
+              {noteCardsData[(currentCardIndex + 1) % noteCardsData.length]}
+            </div>
+          </div>
+          <div className="carousel-arrows">
+            <span className="arrow" onClick={prevCard}>&#10094;</span>
+            <span className="arrow" onClick={nextCard}>&#10095;</span>
+          </div>
+        </>
+      ) : (
+        <NoteCardGrid noteCardsData={noteCardsData} />
+      )}
+
+      <div className="menu-button" onClick={toggleViewMode}>
+        {viewMode === 'carousel' ? 'Switch to Grid View' : 'Switch to Carousel View'}
       </div>
     </div>
   );
